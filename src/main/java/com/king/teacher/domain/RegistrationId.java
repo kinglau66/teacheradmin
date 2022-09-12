d1179ffecefa61fea14f2abd0cdc01fed5339998
King <kinglau66@hotmail.com>
2022-09-13 06:41:29 +0800

    fix: missing delombok

--- src/main/java/com/king/teacher/domain/RegistrationId.java
+++ src/main/java/com/king/teacher/domain/RegistrationId.java
@@ -1,21 +1,31 @@
 package com.king.teacher.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
-import javax.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
 public class RegistrationId implements Serializable {
 
     private Teacher teacher;
     private Student student;
+
+    public RegistrationId() {}
+
+    public Teacher getTeacher() {
+        return this.teacher;
+    }
+
+    public Student getStudent() {
+        return this.student;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String toString() {
+        return "RegistrationId(teacher=" + this.getTeacher() + ", student=" + this.getStudent() + ")";
+    }
 }
